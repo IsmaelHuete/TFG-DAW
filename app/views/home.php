e8b9c5d3300b79b855dc0002126d096cf7877e74
Ismael <ismaelhuete720@gmail.com>
2025-03-18 21:53:10 +0100

    Implementacion index

--- app/views/home.php
+++ app/views/home.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYpE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -16,8 +16,120 @@
     <?php 
         include ("layouts/header.php");
     ?>
-    <main></main>
+    <main>
+        <section class="section-1">
+            <h1>ASIDE</h1>
+        </section>
 
+        <section class="section-2">
+            <div class="artist-populares">
+                <div class="header-sections">
+                    <h2>Artistas populares</h2>
+                    <span>Ver mas</span>
+                </div>
+                <div class="artistas">
+                    <div class="artista">
+                        <img src="img/AnueDobleA.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="artista">
+                        <img src="img/ConejoMalo.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="artista">
+                        <img src="img/AngelCaido.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="artista">
+                        <img src="img/AnueDobleA.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="artista">
+                        <img src="img/AngelCaido.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    
+                   
+                </div>
+            </div>
+            <div class="albums-populares">
+                <div class="header-sections">
+                    <h2>Albums y singels populares</h2>
+                    <span>Ver mas</span>
+                </div>
+                <div class="albums">
+                    <div class="album">
+                        <img src="img/AnueDobleA.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="album">
+                        <img src="img/ConejoMalo.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="album">
+                        <img src="img/AngelCaido.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="album">
+                        <img src="img/AnueDobleA.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="album">
+                        <img src="img/AngelCaido.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                </div>
+            </div>
+            <div class="novedades-populares">
+                <div class="header-sections">
+                    <h2>Novedad</h2>
+                    <span>Ver mas</span>
+                </div>
+                <div class="novedades">
+                    <div class="novedad">
+                        <img src="img/AnueDobleA.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="novedad">
+                        <img src="img/ConejoMalo.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="novedad">
+                        <img src="img/AngelCaido.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="novedad">
+                        <img src="img/AnueDobleA.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    <div class="novedad">
+                        <img src="img/AngelCaido.jpg">
+                        <p>Anuel Doble AA</p>
+                        <p>Artist</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
 
+    </main>
+
+    <?php 
+        include ("layouts/footer.php");
+    ?>
 </body>
 </html>
---
+++ app/views/layouts/footer.php
@@ -0,0 +1,50 @@
+<footer class="bg-black text-white py-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 ">
+                <h5>Enlaces</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-dark text-decoration-none">Sobre Nosotros</a></li>
+                        <li><a href="#" class="text-dark text-decoration-none">Términos y Condiciones</a></li>
+                        <li><a href="#" class="text-dark text-decoration-none">Política de Privacidad</a></li>
+                        <li><a href="#" class="text-dark text-decoration-none">Soporte</a></li>
+                    </ul>
+            </div>
+            <div class="col-md-3 ">
+                <h5>Enlaces</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-dark text-decoration-none">Sobre Nosotros</a></li>
+                        <li><a href="#" class="text-dark text-decoration-none">Términos y Condiciones</a></li>
+                        <li><a href="#" class="text-dark text-decoration-none">Política de Privacidad</a></li>
+                        <li><a href="#" class="text-dark text-decoration-none">Soporte</a></li>
+                    </ul>
+            </div>
+            <div class="col-md-3 ">
+                <h5>Ayuda</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-dark text-decoration-none">Preguntas Frecuentes</a></li>
+                    <li><a href="#" class="text-dark text-decoration-none">Cómo Comprar</a></li>
+                    <li><a href="#" class="text-dark text-decoration-none">Métodos de Pago</a></li>
+                    <li><a href="#" class="text-dark text-decoration-none">Devoluciones</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 ">
+                <a href="#" class="text-white mx-2"><i class="fab fa-facebook fa-2x"></i></a>
+                <a href="#" class="text-white mx-2"><i class="fab fa-twitter fa-2x"></i></a>
+                <a href="#" class="text-white mx-2"><i class="fab fa-instagram fa-2x"></i></a>
+                <a href="#" class="text-white mx-2"><i class="fab fa-youtube fa-2x"></i></a>
+            </div>
+        </div>
+        <div class="text-center mt-3">
+            <p class="mb-0 text-dark">&copy; TFG-DAW2-Ismael-Huete-Marcos-Guerrero-Alonso-Esteban</p>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+    crossorigin="anonymous"></script>
