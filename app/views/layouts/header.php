70f15ff6b3022c6f1671214c1e06b089cee77857
Ismael <ismaelhuete720@gmail.com>
2025-03-22 21:51:40 +0100

    Implementacion index

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -1,5 +1,5 @@
 <nav>
-    <img src="/img/image-removebg-preview.png">  
+    <img src="/img/image-removebg-preview (2).png">  
     <div class="menu">
         <ul>
             <li><a href="/myMusic">My Music</a></li>
@@ -12,3 +12,25 @@
         </ul>
     </div>
 </nav>
+
+<div id="carouselExampleFade" class="carousel slide carousel-fade">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/img" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/img" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/img" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
