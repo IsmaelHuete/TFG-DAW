4201419977f428dc038b9cdd09547703942acf3f
Marcos <marcosgclase@gmail.com>
2025-05-12 23:41:17 +0200

    Subir Cancion

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ismae/tfg-daw-ismael-huete-marcos-guerro-alonso-esteban',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b52e2d9a7064f99668158054835ad08b61e49df9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ismae/tfg-daw-ismael-huete-marcos-guerro-alonso-esteban' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b52e2d9a7064f99668158054835ad08b61e49df9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'james-heinrich/getid3' => array(
+            'pretty_version' => 'v1.9.23',
+            'version' => '1.9.23.0',
+            'reference' => '06c7482532ff2b3f9111b011d880ca6699c8542b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../james-heinrich/getid3',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
